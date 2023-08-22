2faa04fc06f9362354e65b1ff392242e9f02b42e
ButchanCrawford <butchan@outlook.com>
2023-08-22 10:37:30 -0700

    Improved game flow from level to level

--- game.py
+++ game.py
@@ -6,13 +6,14 @@ hint2 = ""
 hint3 = ""
 lives = 100
 current_level = -1
-failed_attempt = True
+successful_attempt = True
 
 
 #user guesses code
 def guess_code():
-    guess_code = input("Enter Password: ")
     
+    guess_code = input("Enter Password: ")
+    print(f"guess the code input func ran {guess_code} was the guess")
     return guess_code
 
 #validate guess against actual code
@@ -28,6 +29,7 @@ def validate_code(guess, actual):
 def run_lvl0():
     global code
     code = "password1"
+    code = "1"
     global hint1
     global hint2
     global hint3
@@ -43,6 +45,7 @@ def run_lvl1():
     global hint2
     global hint3
     code = "oprahWIN$1954"
+    code = "1"
     hint1 = "A combination of personal information with a slight twist."
     hint2 = "Letters: Y | Numbers: Y | Special Characters: Y | Upper And Lower: Y | Spaces: N"
     hint3 = "13 Characters"
@@ -52,6 +55,7 @@ def run_lvl1():
 def run_lvl2():
     global code
     code = "BUTwaitTHEREisMORE!"
+    code = "1"
     global hint1
     global hint2
     global hint3
@@ -62,23 +66,35 @@ def run_lvl2():
     print("Name: Billey Mays \n DOB:6/20/1958 \n Occupation: Salesman \n")
 
 #select the game stage / level
-def level_selector():
+def play_again():
+    print("play again func?")#debug
     continue_play = input("Attempt to crack a password? Y or Yes to Continue, any other input to Stop. ").upper()
-    global failed_attempt
-    if  True == failed_attempt and continue_play == "Y" or continue_play == "YES":
-        print(f"Level up \nfail attempt state {failed_attempt}")
+    print(f"continue play after input  {continue_play}") #debug #debug #debug
+    global successful_attempt
+    if  True == successful_attempt and continue_play == "Y" or continue_play == "YES":
+        print(f"Level up \nfail attempt state: {successful_attempt}")
         global current_level
         current_level += 1
-    
-    if current_level == 0:
-        run_lvl0()
-    elif current_level == 1:
-        run_lvl1()
-    elif current_level == 2:
-        run_lvl2()
-    else:
+    elif False == successful_attempt and continue_play == "Y" or continue_play == "YES":
+        print("goint to run level selector func")
+    elif continue_play != "Y" or continue_play != "YES":
+        print("Password Guesser Stopped Running")
+        print(f"continue play when != Y YES:  {continue_play}") #debug #debug #debug
         global game_on
         game_on = False
+
+def level_selector():
+    print("level selector")#debug
+    global game_on
+    if current_level == 0 and game_on == True:
+        run_lvl0()
+    elif current_level == 1 and game_on == True:
+        run_lvl1()
+    elif current_level == 2 and game_on == True:
+        run_lvl2()
+    elif current_level > 2: 
+        game_on = False
+        print(f"All Levels Completed. Congrats!!")
 
 def game_state():
     gaming = input("Play Again? Y or Yes to Continue, any other input to Stop.")
@@ -89,20 +105,26 @@ def game_state():
 
 def game():
     print(f"Welcome to Password Guesser \nYou have {lives} tries to guess the password.")
-    
+    global game_on
     while game_on:
+        print("WHILE LOOP RESTARTED")
+        play_again()
         level_selector()
         guess = guess_code()
         validate_code(guess, code)
         if guess != code:
-           global failed_attempt
-           failed_attempt = False
-           print("failed_attempt at password")
-           
-        else:
-         failed_attempt = True
-         level_selector()
+           global successful_attempt
+           successful_attempt = False
+           print("failed_attempt at password") 
         
+        elif guess == code and current_level >= 2:
+            
+            game_on = False
+            print("All Levels Completed")
+        else:
+         print("success_attempt at password") #debug
+         successful_attempt = True
+       
 
 if __name__ == "__main__":
     game()
